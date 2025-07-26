684dbd6134a93de9af5853261e2f6d013696cf10
Peter Wolf <84736182+peterwolf-pl@users.noreply.github.com>
2025-07-26 23:16:38 +0200

    Enable PHP error display

--- db.php
+++ db.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $servername = "localhost";  // Update to your server details
 $usernames = "srv51934_mkal_inwentarz";
 $passwords = "2akvAaHNNk9Ey4nYGpHs";
